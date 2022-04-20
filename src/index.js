1102dc2733b9838f571eebb959eac02b932bc680
wc-su <theblissout@gmail.com>
2022-04-20 12:21:06 +0800

    add tool.js(include link css, fetch)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 // header color change
 import headerColorChange from "./modules/headerColorChange.js";
+import { linkCss } from "./modules/tool.js";
+
+linkCss("base.css");
+linkCss("layout.css");
+linkCss("component.css");
