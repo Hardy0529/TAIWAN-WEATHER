48afbd3ab29dd21caaef3ecb14250929eb3b6a4f
李杰儒 Jeff <ijieru41@gmail.com>
2022-04-21 01:33:56 +0800

    updata

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // header color change
 import headerColorChange from "./modules/headerColorChange.js";
-<<<<<<< HEAD
 
 // Transform Cloud
 import transformCloud from "./modules/transformCloud.js";
-=======
+
 import GPS from "./modules/GPS.js";
 import dropDownMenu from "./modules/dropDownMenu.js";
 import renderEeatherData from "./modules/renderWeatherData.js";
@@ -12,5 +11,3 @@ import { linkCss } from "./modules/tool.js";
 linkCss("base.css");
 linkCss("layout.css");
 linkCss("component.css");
-
->>>>>>> 332528d1146501ec3a12e6a1e139d4bff203b102
