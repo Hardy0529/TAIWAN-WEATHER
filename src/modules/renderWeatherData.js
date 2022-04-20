5d7b6af207f4c4d3b3fa050fef5f090f2f363935
wc-su <theblissout@gmail.com>
2022-04-20 18:09:30 +0800

    fix: adjust week info

--- src/modules/renderWeatherData.js
+++ src/modules/renderWeatherData.js
@@ -82,13 +82,13 @@ function render36HoursWeather() {
 }
 
 const weekConfig = {
-    "0": "MON",
-    "1": "TUE",
-    "2": "WED",
-    "3": "THU",
-    "4": "FRI",
-    "5": "SAT",
-    "6": "SUN",
+    "0": "SUN",
+    "1": "MON",
+    "2": "TUE",
+    "3": "WED",
+    "4": "THU",
+    "5": "FRI",
+    "6": "SAT",
 }
 
 function renderWeekWeather(locationName, container) {
@@ -98,7 +98,7 @@ function renderWeekWeather(locationName, container) {
         const weatherWeekItem = document.createElement("div");
         weatherWeekItem.classList.add("weather__weekWeather-item");
 
-        const day = new Date(weatherData.time[i]).getUTCDay();
+        const day = new Date(weatherData.time[i]).getDay();
         const weatherWeekItemDay = document.createElement("h3");
         weatherWeekItemDay.textContent = weekConfig[day];
         
