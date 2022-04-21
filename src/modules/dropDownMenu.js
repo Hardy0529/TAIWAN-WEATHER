921066660dad501a0fa6f6258384aecf191d95ac
chiachil <65595327+chiachil@users.noreply.github.com>
2022-04-21 13:27:56 +0800

    feat: update drop-down-menu

--- src/modules/dropDownMenu.js
+++ src/modules/dropDownMenu.js
@@ -1,22 +1,35 @@
 let dropDownMenuBar = document.querySelector(".weather__menu-bar");
-let dropDownMenuBtn = document.querySelector(".weather__menu-bar-btn");
 let dropDownMenuOptions = document.createElement("div");
 let cities = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市", "基隆市", "新竹市", "嘉義市", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "屏東縣", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"];
 
-// only for test, need to delete before production
-let close = document.querySelector("#close");
+eventListenMenu();
 
-// when user clicks drop-down menu, render menu
-dropDownMenuBar.addEventListener("click", (e) => {
-    if (e.target == dropDownMenuBtn){
-        renderOptions();
-    }
-})
-
-// when user clicks close, remove menu
-close.addEventListener("click", () => {
-    removeOptions();
-})
+export function eventListenMenu() {
+    dropDownMenuBar.addEventListener("click", (e) => {
+        let dropDownMenuIcon = document.querySelector("#weather__menu-bar-icon");
+        if (e.target.className == "icon-down-open-big"){
+            renderOptions();
+            dropDownMenuIcon.classList.remove("icon-down-open-big");
+            dropDownMenuIcon.classList.add("icon-up-open-big");
+            e.stopPropagation();
+            return;
+        }
+        if (e.target.className == "icon-up-open-big"){
+            removeOptions();
+            dropDownMenuIcon.classList.remove("icon-up-open-big");
+            dropDownMenuIcon.classList.add("icon-down-open-big");
+            e.stopPropagation();
+            return;
+        }
+        if (e.target.className == "weather__menu-bar-option"){
+            let cityName = e.target.value;
+            removeOptions();
+            dropDownMenuIcon.classList.remove("icon-up-open-big");
+            dropDownMenuIcon.classList.add("icon-down-open-big");
+            return cityName;
+        }
+    }, false)
+}
 
 function renderOptions() {
     dropDownMenuOptions.classList.add("weather__menu-bar-options");
@@ -24,7 +37,7 @@ function renderOptions() {
     if (dropDownMenuOptions.firstChild){
         return;
     } else {
-        for(i = 0; i < cities.length; i++){
+        for(let i = 0; i < cities.length; i++){
             let city = cities[i];
             let dropDownMenuOption = document.createElement("button");
             dropDownMenuOption.classList.add("weather__menu-bar-option");
