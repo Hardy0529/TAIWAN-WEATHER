5ded76fb5f796f1b8314c29d9824f0d4be90be1f
wc-su <theblissout@gmail.com>
2022-04-21 10:14:27 +0800

    add function: refresh weather data

--- src/modules/renderWeatherData.js
+++ src/modules/renderWeatherData.js
@@ -1,4 +1,4 @@
-import { getWeekWeatherData, get36HoursWeatherData } from "./tool.js";
+import { getWeatherDataWeek, getWeatherData36Hours } from "./tool.js";
 import { getWeatherSVG } from "./weather-svg.js"
 
 let hoursWeatherData = null;
@@ -6,25 +6,36 @@ let weekWeatherData = null;
 
 const unitWeatherOfPage = 3;
 let weatherNum = 0;
+const weatherContainer = document.querySelector(".weather__render");
+const weatherObserver = document.querySelector(".weather__render-observer");
+const timeRefresh = document.querySelector(".weather__UpdateTime > span");
+const iconRefresh = document.querySelector(".icon-arrows-cw");
 // 設定監聽條件
 const observerCallback = ([entry]) => {
     if (entry && entry.isIntersecting) {
-        render36HoursWeather();
+        renderWeather();
         weatherNum++;
     }
 };
-
-const weatherContainer = document.querySelector(".weather__render");
-const weatherObserver = document.querySelector(".weather__render-observer");
 // 建立一個 intersection observer
 const observer = new IntersectionObserver(observerCallback);
 
-function render36HoursWeather() {
-    const startIndex = unitWeatherOfPage * weatherNum;
-    const maxLength = Math.min(hoursWeatherData.length, unitWeatherOfPage * (weatherNum + 1));
+
+export function renderWeather(locationName=null) {
+    let startIndex = unitWeatherOfPage * weatherNum;
+    let maxLength = Math.min(hoursWeatherData.length, unitWeatherOfPage * (weatherNum + 1));
+    if(locationName) {
+        startIndex = hoursWeatherData.findIndex((element) => element.locationName == locationName);
+        maxLength = startIndex + 1;
+        observer.unobserve(weatherObserver);
+    }
     if(startIndex > maxLength) {
         observer.unobserve(weatherObserver);
     }
+    renderWeather36Hours(startIndex, maxLength, locationName);
+}
+
+function renderWeather36Hours(startIndex, maxLength, locationName=null) {
     for(let i = startIndex; i < maxLength; i++) {
         const weatherData=hoursWeatherData[i]
         const label = document.createElement("label");
@@ -36,7 +47,7 @@ function render36HoursWeather() {
         // weekinfo
         const weatherWeekWeather = document.createElement("div");
         weatherWeekWeather.classList.add("weather__weekWeather");
-        renderWeekWeather(weatherData.locationName, weatherWeekWeather);
+        renderWeatherWeek(weatherData.locationName, weatherWeekWeather);
 
         const weatherInfoCountry = document.createElement("div");
         weatherInfoCountry.textContent = weatherData.locationName;
@@ -105,7 +116,7 @@ const weekConfig = {
     "6": "SAT",
 }
 
-function renderWeekWeather(locationName, container) {
+function renderWeatherWeek(locationName, container) {
     const index = weekWeatherData.findIndex((element) => element.locationName == locationName);
     const weatherData = weekWeatherData[index];
     for(let i = 0; i < weatherData.time.length; i++) {
@@ -123,13 +134,22 @@ function renderWeekWeather(locationName, container) {
     }
 }
 
+async function init() {
+    hoursWeatherData = await getWeatherData36Hours();
+    weekWeatherData = await getWeatherDataWeek();
+    // 資料載入，開始監測
+    observer.observe(weatherObserver);
+    // 更新畫面時間
+    const now = new Date();
+    timeRefresh.textContent = `更新時間：${now.getMonth()}/${now.getDate()} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes()}:${now.getSeconds().toString().padStart(2, '0')}`;
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
     init();
 });
 
-async function init() {
-    hoursWeatherData = await get36HoursWeatherData();
-    weekWeatherData = await getWeekWeatherData();
-    // 資料載入，開始監測
-    observer.observe(weatherObserver);
-}
+iconRefresh.addEventListener("click", (e) => {
+    e.preventDefault();
+    init();
+});
