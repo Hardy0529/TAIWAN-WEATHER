fe9be314035a9d7b1f3debbcf5d5a294175436f5
chiachil <65595327+chiachil@users.noreply.github.com>
2022-04-21 15:51:35 +0800

    feat: add show all cities' weather in menu

--- src/modules/dropDownMenu.js
+++ src/modules/dropDownMenu.js
@@ -2,7 +2,7 @@ import { renderWeatherLocationLock } from "./renderWeatherData";
 
 let dropDownMenuBar = document.querySelector(".weather__menu-bar");
 let dropDownMenuOptions = document.createElement("div");
-let cities = ["臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市", "基隆市", "新竹市", "嘉義市", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "屏東縣", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"];
+let cities = ["全臺縣市","臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市", "基隆市", "新竹市", "嘉義市", "新竹縣", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "屏東縣", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"];
 
 
 dropDownMenuBar.addEventListener("click", (e) => {
